88098816bfbc2193c55764c81e1af185cc61e034
MyungJun-Youn <yun0244@naver.com>
2017-03-09 13:41:52 +0900

    DataUtil 수정
     - exception으로 처리하는 부분 삭제
     - name을 list로 받아와 존재하는지 확인

--- JAVA/DataUtil.java
+++ JAVA/DataUtil.java
@@ -13,178 +13,177 @@ import org.springframework.util.NumberUtils;
 
 public class DataUtil {
 
-	private static boolean equals(Field src, Field target) {
-		if( src == null || target == null) {
-			return false;
-		}
-		return src.getName().equals(target.getName()) && src.getType().equals(target.getType());
-	}
-
 	/**
-	 * Object의 데이터를 옮기기 위한 함수
-	 * 
-	 * @param srcObj
-	 * 			소스 데이터
-	 * @param targetObj
-	 * 			결과 데이터
-	 */
-	public static void converterData(Object srcObj, final Object targetObj) {
-		Class<?> srcClass = srcObj.getClass();
-		Class<?> targetClass = targetObj.getClass();
-
-		boolean srcAccessible = false;
-		boolean targetAccessible = false;
-		Field srcField = null;
-
-		boolean targetChanged = false;
-		
-		for (Field targetField : targetClass.getDeclaredFields()) {
-			try {
-				srcField = srcClass.getDeclaredField(targetField.getName());
-				srcAccessible = srcField.isAccessible();
-				if( !equals(srcField, targetField)){
-					continue;
-				}
-
-				targetAccessible = targetField.isAccessible();
-				targetChanged = true;
-
-				srcField.setAccessible(true);
-				targetField.setAccessible(true);
-
-				targetField.set(targetObj, srcField.get(srcObj));
-
-			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-//				e.printStackTrace();
-			} finally {
-				if (srcField != null) {
-					srcField.setAccessible(srcAccessible);
-
-					srcField = null;
-				}
-
-				if( targetChanged ) {
-					targetField.setAccessible(targetAccessible);
-
-					targetChanged = false;
-				}
-			}
-		}
-	}
- 
-	/**
-	 * 데이터를 다른 타입으로 변환하기 위한 함수
-	 * 
+	 * 데이터를 다른 타입의 데이터로 변환하기 위한 함수
+	 * ex> VO to DTO, DTO to VO
+	 *
 	 * @param obj
-	 * 			데이터
+	 *            데이터
 	 * @param genericType
-	 * 			변환하기 위한 타입
+	 *            변환하기 위한 타입
 	 * @return 변환한 데이터
 	 */
-	public static <T> T converterData(Object obj, Class<T> genericType) {
+	public static <T> T converterDataToData(Object obj, Class<T> genericType) {
 		T t = null;
+
 		try {
 			t = genericType.newInstance();
-			converterData(obj, t);
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
+			converterDataToData(obj, t);
+		} catch (IllegalArgumentException | IllegalAccessException | InstantiationException | NoSuchFieldException
+				| SecurityException e) {
 			e.printStackTrace();
 		}
+
 		return t;
 	}
 
 	/**
-	 * 맵 데이터를 Object로 저장하기 위한 함수
-	 * 
-	 * @param srcMap
-	 * 			소스 데이터
-	 * @param targetObject
-	 * 			변환하기 위한 타입
+	 * Object의 데이터를 옮기기 위한 함수
+	 *
+	 * @param srcObj
+	 *            소스 데이터
+	 * @param targetObj
+	 *            결과 데이터
+	 * @throws SecurityException
+	 * @throws NoSuchFieldException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
 	 */
-	public static void mapConvertToDataObject(Map<?, ?> srcMap, Object targetObject) {
-		Field targetField = null;
-		boolean targetAccessible = false;
+	private static void converterDataToData(Object srcObj, final Object targetObj)
+			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Class<?> srcClass = srcObj.getClass();
+		Class<?> targetClass = targetObj.getClass();
 
-		try {
-			Set<?> keys = srcMap.keySet();
-			Iterator<?> keyIterator = keys.iterator();
-			Class<?> targetClass = targetObject.getClass();
+		Field[] srcFields = srcClass.getDeclaredFields();
+		Field[] targetFields = targetClass.getDeclaredFields();
 
-			Field[] fields = targetClass.getDeclaredFields();
+		@SuppressWarnings("unchecked")
+		List<String> srcFieldNames = (List<String>) Arrays.asList(srcFields).stream().map(srcField -> (srcField.getName()));
+		@SuppressWarnings("unchecked")
+		List<String> targetFieldNames = (List<String>) Arrays.asList(targetFields).stream().map(targetField -> (targetField.getName()));
 
-			@SuppressWarnings("unchecked")
-			List<String> fieldNames = (List<String>) Arrays.asList(fields)
-															.stream()
-															.map(field -> {
-																return field.getName();
-															});
+		Field srcField;
+		boolean srcAccessible;
+		Field targetField;
+		boolean targetAccessible;
 
-			while (keyIterator.hasNext()) {
-				Object keyName = keyIterator.next();
-				Object value = srcMap.get(keyName);
+		for (String srcFieldName : srcFieldNames) {
+			if (targetFieldNames.contains(srcFieldName) == true) {
+				srcField = srcClass.getDeclaredField(srcFieldName);
+				srcAccessible = srcField.isAccessible();
 
-				boolean checkIncludeField = fieldNames.contains(keyName.toString());
+				targetField = targetClass.getDeclaredField(srcFieldName);
+				targetAccessible = targetField.isAccessible();
 
-				if (checkIncludeField) {
-					try {
-						targetField = targetClass.getDeclaredField(keyName.toString());
-						targetAccessible = targetField.isAccessible();
+				if (equals(srcField, targetField) == false) {
+					continue;
+				} else {
+					srcField.setAccessible(true);
+					targetField.setAccessible(true);
 
-						targetField.setAccessible(true);
+					targetField.set(targetObj, srcField.get(srcObj));
 
-						if (targetField.getType() == int.class || targetField.getType() == Integer.class) {
-							value = NumberUtils.parseNumber((String)value, Integer.class);
-						} else if (targetField.getType() == long.class || targetField.getType() == Long.class) {
-							value = NumberUtils.parseNumber((String)value, Long.class);
-						} else if (targetField.getType() == float.class || targetField.getType() == Float.class) {
-							value = NumberUtils.parseNumber((String)value, Float.class);
-						} else if (targetField.getType() == boolean.class || targetField.getType() == Boolean.class) {
-							value = BooleanUtils.toBoolean((String)value);
-						} else if (targetField.getType() == Date.class) {
-							value = new Date(NumberUtils.parseNumber((String)value, Long.class));
-						} else {
-							value = (String) value;
-						}
+					srcField.setAccessible(srcAccessible);
+					targetField.setAccessible(targetAccessible);
 
-						targetField.set(targetObject, value);
-					} catch (NoSuchFieldException | SecurityException | IllegalArgumentException e) {
-						e.printStackTrace();
-					} finally {
-						if (targetField != null) {
-							targetField.setAccessible(targetAccessible);
-							targetField = null;
-						}
-					}
+					srcField = null;
+					targetField = null;
 				}
 			}
-		} catch (IllegalAccessException | IllegalArgumentException e) {
-			e.printStackTrace();
 		}
 	}
 
 	/**
 	 * 맵 데이터를 원하는 타입으로 변환하기 위한 함수
-	 * 
+	 *
 	 * @param srcMap
-	 * 			소스 맵 데이터
+	 *            소스 맵 데이터
 	 * @param genericType
-	 * 			변환하려는 타입
+	 *            변환하려는 타입
 	 * @return 변환한 데이터
 	 */
-	public static <T> T mapConvertToDataObject(Map<?, ?> srcMap, Class<T> genericType) {
+	public static <T> T convertMapToData(Map<?, ?> srcMap, Class<T> genericType) {
 		T t = null;
 
 		try {
 			t = genericType.newInstance();
-			mapConvertToDataObject(srcMap, t);
-		} catch (InstantiationException | IllegalAccessException e) {
+			convertMapToData(srcMap, t);
+		} catch (InstantiationException | IllegalAccessException | NoSuchFieldException | SecurityException
+				| IllegalArgumentException e) {
 			e.printStackTrace();
 		}
 
 		return t;
 	}
 
+	/**
+	 * 맵 데이터를 Object로 저장하기 위한 함수
+	 *
+	 * @param srcMap
+	 *            소스 데이터
+	 * @param targetObject
+	 *            변환하기 위한 타입
+	 * @throws SecurityException
+	 * @throws NoSuchFieldException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 */
+	private static void convertMapToData(Map<?, ?> srcMap, Object targetObject)
+			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field targetField = null;
+		boolean targetAccessible = false;
+
+		Set<?> keys = srcMap.keySet();
+		Iterator<?> keyIterator = keys.iterator();
+		Class<?> targetClass = targetObject.getClass();
+
+		Field[] fields = targetClass.getDeclaredFields();
+
+		@SuppressWarnings("unchecked")
+		List<String> fieldNames = (List<String>) Arrays.asList(fields).stream().map(field -> (field.getName()));
+
+		while (keyIterator.hasNext()) {
+			Object keyName = keyIterator.next();
+			Object value = srcMap.get(keyName);
+
+			if (fieldNames.contains(keyName.toString()) == true) {
+				targetField = targetClass.getDeclaredField(keyName.toString());
+				targetAccessible = targetField.isAccessible();
+
+				targetField.setAccessible(true);
+
+				value = convertObject(value, targetField);
+
+				targetField.set(targetObject, value);
+
+				targetField.setAccessible(targetAccessible);
+				targetField = null;
+			}
+		}
+	}
+
+	private static boolean equals(Field src, Field target) {
+		if (src == null || target == null) {
+			return false;
+		}
+		return src.getName().equals(target.getName()) && src.getType().equals(target.getType());
+	}
+
+	private static Object convertObject(Object value, Field field) {
+		if (field.getType() == int.class || field.getType() == Integer.class) {
+			value = NumberUtils.parseNumber((String) value, Integer.class);
+		} else if (field.getType() == long.class || field.getType() == Long.class) {
+			value = NumberUtils.parseNumber((String) value, Long.class);
+		} else if (field.getType() == float.class || field.getType() == Float.class) {
+			value = NumberUtils.parseNumber((String) value, Float.class);
+		} else if (field.getType() == boolean.class || field.getType() == Boolean.class) {
+			value = BooleanUtils.toBoolean((String) value);
+		} else if (field.getType() == Date.class) {
+			value = new Date(NumberUtils.parseNumber((String) value, Long.class));
+		} else {
+			value = (String) value;
+		}
+
+		return value;
+	}
 }
